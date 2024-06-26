517808a31ec90670bcc36b4472242514cd164092
Federico Gaubeca <47962250@est.ort.edu.ar>
2024-06-26 17:51:38 -0300

    Basic Setup
    Added UI.
    Implemented a product randomizer and products prices.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Producto[] productosIzquierda;
+    public Producto[] productosDerecha;
+    public Text productLPriceText;
+    public Text productRPriceText;
+
+    private Producto productoIzquierdaActual;
+    private Producto productoDerechaActual;
+    void Start()
+    {
+        DeactivateAllProducts();
+        GenerateRandomProduct();
+    }
+
+    void DeactivateAllProducts()
+    {
+        foreach (var producto in productosIzquierda)
+        {
+            producto.gameObject.SetActive(false);
+        }
+
+        foreach (var producto in productosDerecha)
+        {
+            producto.gameObject.SetActive(false);
+        }
+    }
+
+    void GenerateRandomProduct()
+    {
+        int indexIzquierda = Random.Range(0, productosIzquierda.Length);
+        int indexDerecha = Random.Range(0, productosDerecha.Length);
+
+        productoIzquierdaActual = productosIzquierda[indexIzquierda];
+        productoDerechaActual = productosDerecha[indexDerecha];
+
+        productoIzquierdaActual.gameObject.SetActive(true);
+        productoDerechaActual.gameObject.SetActive(true);
+
+        productLPriceText.text = productoIzquierdaActual.precio.ToString();
+        productRPriceText.text = productoDerechaActual.precio.ToString();
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Text userAnswerText;
+    public Button respondButton;
+    public GameObject notificationPanel;
+    public GameObject incorrectText;
+    public GameObject correctText;
+    public Button retryButton;
+    public Button quitButton;
+
+    void Start()
+    {
+        notificationPanel.SetActive(false);
+    }
+}
